13714f7a777bbf504f964b6e6667920654a48f5c
minmg285 <mthmaung0001@gmail.com>
2020-03-20 17:12:46 +0630

    login page background added

--- src/App.js
+++ src/App.js
@@ -2,18 +2,24 @@ import React from "react";
 import PostIcon from "@material-ui/icons/Book";
 import UserIcon from "@material-ui/icons/Group";
 import jsonServerProvider from "ra-data-json-server";
-import { Admin, Resource } from "react-admin";
+import { Admin, Resource, Login } from "react-admin";
 import { UserList } from "./users";
 import { PostList, PostCreate, PostEdit } from "./posts";
 import Dashboard from "./Dashboard";
 import authProvider from "./authProvider";
 const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
-
+const MyLoginPage = () => (
+  <Login
+    // A random image that changes everyday
+    backgroundImage="https://source.unsplash.com/random/1600x900/daily"
+  />
+);
 const App = () => (
   <Admin
     dashboard={Dashboard}
     dataProvider={dataProvider}
     authProvider={authProvider}
+    loginPage={MyLoginPage}
   >
     <Resource
       name="posts"
